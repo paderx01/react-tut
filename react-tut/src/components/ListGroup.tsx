68accd5221994e4d31c0140811a43c43c3ddd9da
Pader <paderx26@gmail.com>
2026-03-09 19:10:21 -0500

    handling events

--- react-tut/src/components/ListGroup.tsx
+++ react-tut/src/components/ListGroup.tsx
@@ -1,13 +1,20 @@
 function ListGroup() {
+  let items = ["New York", "San Franciso", "Minneapolis", "London", "Paris"];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Clicked")}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
