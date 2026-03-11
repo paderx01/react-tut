dfa7e25069871dae6aa77eb5034e803ce7c67759
Pader <paderx26@gmail.com>
2026-03-11 13:16:15 -0500

    useState - handling state mgmt

--- react-tut/src/components/ListGroup.tsx
+++ react-tut/src/components/ListGroup.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Franciso", "Minneapolis", "London", "Paris"];
+  let selectedIndex = 0;
+  const arr = useState(-1);
+  arr[0]; //variable (selectedIndex)
+  arr[1]; //updater function
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log("Clicked")}
+            onClick={() => {
+              selectedIndex = index;
+            }}
           >
             {item}
           </li>
